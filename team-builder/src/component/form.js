4abd37cc3ea96c33a9294ec9d523d1c81b03ecfb
beefybroccoli <tedkimemail@yahoo.com>
2021-09-13 08:58:00 -0700

    no comment

--- team-builder/src/component/form.js
+++ team-builder/src/component/form.js
@@ -1,9 +1,39 @@
 import React, { useState, useEffect } from "react";
 
 export default function Form(props) {
+  const initial_state = {
+    name: "",
+    role: "",
+    email: "",
+  };
+
+  const [stateForm, set_stateForm] = useState(initial_state);
+  const [stateError, set_stateError] = useState(null);
+
+  const cb_onChange = (event) => {
+    console.log("event.target = ", event.target);
+    console.log("event.value = ", event.value);
+  };
+
+  const cb_onSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
       <h3>Form.js</h3>
+      <form onSubmit={cb_onSubmit}>
+        <label>
+          Name :{" "}
+          <input
+            name=""
+            type="text"
+            value=""
+            id="input_name"
+            onChange={cb_onChange}
+          />
+        </label>
+      </form>
     </div>
   );
 }
