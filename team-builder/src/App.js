c80647fc4da3691e86196645269fb3f2dbbb0d66
beefybroccoli <tedkimemail@yahoo.com>
2021-09-13 08:51:38 -0700

    no comment

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,16 +1,44 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Form from "./component/form.js";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const intial_state = [
+    { name: "tom", role: "manager", email: "tom@gmail.com" },
+  ];
+  const [stateTeamMembers, set_stateTeamMember] = useState(intial_state);
+  const [stateNewMember, set_stateNewMember] = useState(null);
+
+  useEffect(() => {
+    if (stateNewMember) {
+      console.log("stateNewMember = ", stateNewMember);
+
+      //add member to Member_List
+      set_stateTeamMember([...stateTeamMembers, stateNewMember]);
+
+      //reset stateNewMember to null
+      set_stateNewMember(null);
+    }
+  }, [stateNewMember]);
+
   return (
     <div className="App">
       <header>
         <h2>Team Builder</h2>
       </header>
       <main>
-        <Form />
+        <Form set_stateNewMember={set_stateNewMember} />
       </main>
+      {stateTeamMembers &&
+        stateTeamMembers.map((eachMember, index) => {
+          return (
+            <p key={index}>
+              <b>Name : </b>
+              {eachMember.name}, <b>Role: </b>
+              {eachMember.role}, <b>Email: </b> {eachMember.email}
+            </p>
+          );
+        })}
     </div>
   );
 }
