c61aaac216bae525057197077e036ab813950866
beefybroccoli <tedkimemail@yahoo.com>
2021-09-13 21:14:47 -0700

    no comment

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
-import Form from "./component/form.js";
 import React, { useState, useEffect } from "react";
 import Team from "./component/team";
 import Dashboard from "./component/dashboard";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
+  <Switch>
+    <Route exact path="/">
+      Home
+    </Route>
+    <Route path="teams/:id">
+      <Team />
+    </Route>
+  </Switch>;
+
   return (
     <div className="App">
       <header>
